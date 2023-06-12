ec661d60e810b17d4b9e52ae45664a9f4a05dc0f
Admin <subashbaidya9@gmail.com>
2023-06-12 11:31:26 +0545

    feat - Feed Page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,60 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+import { data } from "autoprefixer";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
 
-export default Feed 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPosts(data);
+      
+    };
+    fetchPosts();
+  }, []);
+
+  
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or prompts...."
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,37 @@
+"use client";
 
-const PromptCard = () => {
+import { useState } from "react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
+const PromptCard = ({ post, handleTagClicked }) => {
+  const [copied, setCopied] = useState("");
+
   return (
-    <div>PromptCard</div>
-  )
-}
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="copy_btn">
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p
+        className='font-inter text-sm blue_gradient cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        #{post.tag}
+      </p>
+    </div>
+  );
+};
 
-export default PromptCard
+export default PromptCard;
