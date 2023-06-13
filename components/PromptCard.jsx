07ef453853100d86001897bb28b9bd3d8fc4af3d
Admin <subashbaidya9@gmail.com>
2023-06-13 16:53:00 +0545

    feat - Prompt Copy Functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,14 @@ import { usePathname, useRouter } from "next/navigation";
 const PromptCard = ({ post, handleTagClicked }) => {
   const [copied, setCopied] = useState("");
 
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => {
+      setCopied("");
+    }, 3000);
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -17,15 +25,16 @@ const PromptCard = ({ post, handleTagClicked }) => {
                 ? "/assets/icons/tick.svg"
                 : "/assets/icons/copy.svg"
             }
+            onClick={handleCopy}
             alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
         </div>
       </div>
-      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
-        className='font-inter text-sm blue_gradient cursor-pointer'
+        className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
         #{post.tag}
